a69de272bf3fe751d028b4a7721c94534268f14c
Hüseyin Adeka <adekahuseyin@hotmail.com>
2020-12-19 00:02:08 +0300

    imona-assignment-1

---
+++ src/main/java/com/adekah/imonaassignment/ImonaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.adekah.imonaassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImonaAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImonaAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adekah/imonaassignment/ImonaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adekah.imonaassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImonaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
